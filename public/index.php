b0c944f736eb96c967de4b4909ea33467704a00a
kristian <kristian_tapia@yahoo.com>
2015-06-24 22:31:23 -0500

    Cors Test6

--- public/index.php
+++ public/index.php
@@ -6,6 +6,11 @@
  * @author   Taylor Otwell <taylorotwell@gmail.com>
  */
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'); // allow certain headers
+
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
