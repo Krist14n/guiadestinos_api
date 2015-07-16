af64a15cceb75efc79f9cc94d0087defbd66fda8
kristian <kristian_tapia@yahoo.com>
2015-07-16 00:24:17 -0500

    test ubicacion3

--- app/Http/Controllers/ApiDestinosUbicacion.php
+++ app/Http/Controllers/ApiDestinosUbicacion.php
@@ -24,8 +24,6 @@ class ApiDestinosUbicacion extends Controller {
 
 		$ubicacion = DB::table('direcciones')
 					->join('restaurantes', 'direcciones.restaurante_id', "=", "restaurantes.id")
-					->join('hoteles', 'direcciones.hotel_id', "=", "hoteles.id")
-					->join('spas', 'direcciones.spa_id', '=','spas.id')
 					->groupby("direcciones.id")
 					->get();
 
