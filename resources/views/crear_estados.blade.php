def16450d85e97ae90ef38904a0013ecf7461d0b
kristian <kristian_tapia@yahoo.com>
2015-07-21 23:11:48 -0500

    Validacion Estados 4

--- resources/views/crear_estados.blade.php
+++ resources/views/crear_estados.blade.php
@@ -16,7 +16,7 @@
 
 		<div class="form-group">
 
-			{!! Form::text('nombre', null, ['class'=> 'form-control']) !!}
+			{!! Form::text('nombre', null, ['class'=> 'form-control','required' => 'required']) !!}
 
 		</div>
 
